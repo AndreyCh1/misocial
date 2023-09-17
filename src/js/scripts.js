da1bb909fb5510d1fba57c195465a91a8b7d9e6f
AndreyCh1 <ganimed3user@gmail.com>
2023-09-17 12:56:05 +0300

    some changes

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,9 +1,6 @@
 function top_menu_toggle() {
     burger.classList.toggle("burger_menu_open")
     topMenu.classList.toggle("top_menu_open")
-    background.classList.toggle("background_open")
     body.classList.toggle("body_no_scroll")
-    footerCopy.classList.toggle("footer-copy")
 }
 burger.onclick = top_menu_toggle
-background.onclick = top_menu_toggle
